710fe2bef485df7252893fb33ea16c7092f8203e
Lalith <lalithchalla111@gmail.com>
2023-10-10 10:29:03 +0530

    New Files Commited

--- src/ConstructorOverloading.java
+++ src/ConstructorOverloading.java
@@ -3,17 +3,17 @@
  * Contact at:lalithcspersonal@gmail.com
  * Date:10-10-2023
  */
-public class MethodOverloading {
-    MethodOverloading(){
+public class ConstructorOverloading {
+    ConstructorOverloading(){
         System.out.println("Default Constructor!");
     }
-    MethodOverloading(int i){
+    ConstructorOverloading(int i){
         System.out.println("Parameterized Constructor");
         System.out.println(i);
     }
 
     public static void main(String[] args) {
-        MethodOverloading obj=new MethodOverloading();
-        MethodOverloading obj1=new MethodOverloading(1);
+        ConstructorOverloading obj=new ConstructorOverloading();
+        ConstructorOverloading obj1=new ConstructorOverloading(1);
     }
 }
